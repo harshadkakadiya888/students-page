731862b009f75bda73897650249d1512e35a2330
harshadkakadiya888 <harshadkakadiya888@gmail.com>
2026-03-05 18:31:13 +0530

    d

--- students_app/urls.py
+++ students_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, Response
 from .views import StudentViewSet
+from .views import home
 
 router = DefaultRouter()
 router.register('students', StudentViewSet)
 
+def home(request):
+    return Response({"message": "Welcome to the Student API!"})
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('',home, name='home'),
 ]
