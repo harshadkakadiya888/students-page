217b2e8710c6fb3719037444aca5004c1b118e61
harshadkakadiya888 <harshadkakadiya888@gmail.com>
2026-03-05 17:44:58 +0530

    add setting

--- students/settings.py
+++ students/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+ALLOWED_HOSTS = ['*']
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
